5f45e23f42f43773970527b9de864770fb2bbc8f
Peter <peter@lundbergnet.se>
2019-11-05 00:53:15 +0100

    Update install.php

--- install.php
+++ install.php
@@ -41,7 +41,7 @@ if ($_POST) {
         echo '<pre>';
         echo $current;
         echo '</pre>';
-        prepend($current,"config1.php");
+        prepend($current,"config.php");
         
       
         
