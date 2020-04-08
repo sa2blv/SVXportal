727434ee966cfd24da39eb3a340f7eb0a653c2e1
Peter <peter@lundbergnet.se>
2020-04-09 00:33:57 +0200

    update config for 2,2

--- config.php
+++ config.php
@@ -12,8 +12,20 @@ $sitename = "2.1";
 $livelink="http://svxportal.sm2ampr.net:8000/svxcard96";
 
 // Svx recording folder
-$dir="/var/www/svx/svxrecording";
+$dir="/var/www/svxrecording";
  
 // svxreflektor server adress;
 $serveradress ="http://sk3w.se/svxreflektor/reflektor%20proxy/";
+
+// login form recording
+//$use_logein= true;
+// Iframe under system description
+//$iframe_documentation_url  ="http://sk3w.se/dokuwiki/doku.php?id=svxreflector&do=export_xhtml";
+
+// Default Position
+$default_lat ="62.676160";
+$default_long ="17.633479";
+
+
+
 ?>
