29cf85a7302cbaf555599b707289dd52245774c3
peteer1991 <peter@lundbergnet.se>
2020-05-22 22:43:55 +0200

    fix
    fix

--- update.php
+++ update.php
@@ -2,10 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$host = "localhost";
-$user ="root";
-$password ="FuraRadio1!";
-$db="testinstall";
+include "config.php";
+
+
 
 
 
