47bbedb19ea56184c44a0c9c5ccb71ed12fc3129
Peter <peter@lundbergnet.se>
2023-08-13 20:18:19 +0200

    Create after.sql

---
+++ sql/after.sql
@@ -0,0 +1,10 @@
+UPDATE `Settings` SET `type` = '3' WHERE `Settings`.`id` = 1; 
+UPDATE `Settings` SET `value` = '2.6' WHERE `Settings`.`id` = 1; 
+
+ALTER TABLE `RefletorStations` DROP INDEX `Callsign_2`;
+
+TRUNCATE `swatportal`.`RefletorStations`;
+ALTER TABLE `RefletorStations` CHANGE `ID` `ID` INT NOT NULL AUTO_INCREMENT; 
+
+ALTER TABLE `RefletorStations` ADD `Version` VARCHAR(200) NOT NULL AFTER `Last_Seen`; 
+ALTER TABLE `RefletorStations` ADD `Sysop` VARCHAR(200) NOT NULL AFTER `Collor`; 
